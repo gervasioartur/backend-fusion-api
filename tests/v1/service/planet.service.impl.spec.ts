a81cb243febfb15d1b978dfc14313385efb59565
gervasioartur <gervasioartur@outlook.com.br>
2024-10-18 11:04:02 -0300

    test: ensure PlanetService throws UnexpectError if save returns null

--- tests/v1/service/planet.service.impl.spec.ts
+++ tests/v1/service/planet.service.impl.spec.ts
@@ -1,8 +1,8 @@
 import {planetFactory} from "@/tests/v1/mocks/planet-mocks";
 import { PlanetService } from "@/v1/services/contract/planet.service";
-import {ConflictError} from "@/v1/domain/error/ConflictError";
+import {Errors, UnexpectError} from "@/v1/domain/errors";
 import {PlanetRepository} from "@/v1/repository/contract/planet.repository";
-import {PlanetServiceImpl} from "../../../src/v1/services/impl/planet.service.impl";
+import {PlanetServiceImpl} from "@/v1/services/impl/planet.service.impl";
 import { mock,Mock } from 'ts-jest-mocker';
 
 
@@ -10,15 +10,17 @@ describe('Planet Service', () => {
     let sut: PlanetService
     let planetRepository: Mock<PlanetRepository>;
 
-    beforeEach(() => {
+    beforeAll(() => {
         planetRepository = mock<PlanetRepository>()
         sut =  new PlanetServiceImpl(planetRepository)
     })
 
+    beforeEach(() => {
+        planetRepository.findByName.mockResolvedValue(null);
+    })
+
     it('Should call findByName with correct values', async () => {
         const params = planetFactory
-
-        planetRepository.findByName.mockResolvedValue(null);
         planetRepository.save.mockResolvedValue(params)
 
         await sut.create(params)
@@ -26,21 +28,28 @@ describe('Planet Service', () => {
         expect(planetRepository.findByName).toHaveBeenCalledTimes(1)
     })
 
-    it('Should throw ConflictError if the planet is already registered', async () => {
+    it('Should throw Errors if the planet is already registered', async () => {
         planetRepository.findByName.mockResolvedValue(planetFactory);
         const promise = sut.create(planetFactory);
-        await  expect(promise).rejects.toThrow(new ConflictError("Planet is already registered!"))
+        await  expect(promise).rejects.toThrow(new Errors("Planet is already registered!"))
     })
 
     it('Should call save with correct values', async () => {
         const params = planetFactory
-
-        planetRepository.findByName.mockResolvedValue(null);
         planetRepository.save.mockResolvedValue(params)
 
         await sut.create(params)
         expect(planetRepository.save).toHaveBeenCalledWith(params)
         expect(planetRepository.save).toHaveBeenCalledTimes(1)
+    })
+
+    it('Should throw UnexpectError if save returns null', async () => {
+        const params = planetFactory
+        planetRepository.save.mockResolvedValue(null)
+
+        const promise = sut.create(params)
+        await expect(promise).rejects.toThrow(
+            new UnexpectError('An unexpected error occurred while trying save planet info.'))
     })
 
 })
