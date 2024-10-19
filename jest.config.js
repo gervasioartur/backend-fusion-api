43480951baf81e1dd40d56d9f5f3c01cf1426f3f
gervasioartur <gervasioartur@outlook.com.br>
2024-10-19 17:55:51 -0300

    chore: configuring code coverage

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,15 @@
 module.exports = {
   coverageProvider: 'babel',
   coverageDirectory: 'coverage',
-  collectCoverageFrom: ['<rootDir>/src/**/*.ts',],
+  collectCoverageFrom: [
+    '<rootDir>/src/**/*.ts',
+    '!<rootDir>/src/app.ts',
+    '!<rootDir>/src/server.ts',
+    '!<rootDir>/src/**/server.ts',
+    '!<rootDir>/src/**/routes.ts',
+    '!<rootDir>/src/**/data-source.ts',
+    '!<rootDir>/src/**/api/factories/*.ts',
+  ],
   moduleNameMapper: {
     '@/tests/(.+)': '<rootDir>/tests/$1',
     '@/(.+)': '<rootDir>/src/$1'
