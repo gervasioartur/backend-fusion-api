bb121afc9bdb3aaf713cbf4c4c3368a0778fbfcb
gervasioartur <gervasioartur@outlook.com.br>
2024-10-19 11:26:54 -0300

    test: ensure PlanetController returns 400 if name is empty or null

---
+++ tests/v1/api/controller/planet.controller.spec.ts
@@ -0,0 +1,60 @@
+import request from 'supertest';
+import express from 'express';
+import { createPlanetFromDtoFactory, createPlanetRequestFactory } from '@/tests/v1/mocks/planet-mocks';
+import { PlanetController } from '@/v1/api/controllers/planet.controller';
+import { PlanetService } from '@/v1/service/contract/planet.service';
+import { createPlanetValidator } from '@/v1/api/validators';
+
+const mockPlanetService = { create: jest.fn() } as unknown as PlanetService;
+
+const createApp = () => {
+  const app = express();
+  app.use(express.json());
+
+  const planetController = new PlanetController(mockPlanetService);
+  app.post('/planets',createPlanetValidator, planetController.create);
+
+  return app;
+};
+
+describe('PlanetController', () => {
+  it('should return 400 if name is empty or null', async () => {
+    const requestParams = createPlanetRequestFactory();
+    requestParams.name = "";
+
+    (mockPlanetService.create as jest.Mock).mockResolvedValue(undefined);
+
+    const app = createApp();
+    const response = await request(app)
+      .post('/planets')
+      .send(requestParams)
+      .expect(400);
+
+    expect(response.body).toEqual({
+      status: 400,
+      message: "Validation Error",
+      body: ["Name is required"]
+    });
+    expect(mockPlanetService.create).toHaveBeenCalledTimes(0)
+  });
+
+  it('should create a new planet and return status 201', async () => {
+    const requestParams = createPlanetRequestFactory();
+    const planet = createPlanetFromDtoFactory(requestParams);
+
+    (mockPlanetService.create as jest.Mock).mockResolvedValue(undefined);
+
+    const app = createApp();
+    const response = await request(app)
+      .post('/planets')
+      .send(requestParams)
+      .expect(201);
+
+    expect(response.body).toEqual({
+      status: 201,
+      message: 'Planet successfully saved!',
+    });
+    expect(mockPlanetService.create).toHaveBeenCalledWith(planet);
+    expect(mockPlanetService.create).toHaveBeenCalledTimes(1)
+  });
+});
--- tests/v1/mocks/planet-mocks.ts
+++ tests/v1/mocks/planet-mocks.ts
@@ -1,8 +1,22 @@
 import {faker} from "@faker-js/faker";
 import { Planet } from '@/v1/domain/entity/planet';
+import { createPlanetRequest } from '@/v1/api/dtos';
 
 export const planetFactory: Planet = new Planet(
     faker.lorem.word(),
     faker.lorem.word(),
     faker.lorem.word(),
     faker.number.int())
+
+export const createPlanetRequestFactory = (): createPlanetRequest => {
+  return {
+    name: faker.lorem.word(),
+    climate: faker.lorem.word(),
+    terrain: faker.lorem.word(),
+    population: faker.number.int(),
+  };
+};
+
+export const createPlanetFromDtoFactory = (requestParams: createPlanetRequest): Planet => {
+  return new Planet(requestParams.name,requestParams.climate,requestParams.terrain,requestParams.population);
+}
