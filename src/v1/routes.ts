f1e1fe5b990bbe53a988b1eaf7198c841b3cc477
gervasioartur <gervasioartur@outlook.com.br>
2024-10-20 20:40:19 -0300

    chore: add delete planet route

--- src/v1/routes.ts
+++ src/v1/routes.ts
@@ -10,5 +10,6 @@ router.post('/planets',createPlanetValidator, planetController.create)
 router.get('/planets', planetController.readAll)
 router.get('/planets/:id', planetController.readById)
 router.put('/planets/:id', planetController.update)
+router.delete('/planets/:id', planetController.delete)
 
 export  default  router
