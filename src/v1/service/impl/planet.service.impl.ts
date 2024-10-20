86dc88f519da9036f77f63406e06924c40aef612
gervasioartur <gervasioartur@outlook.com.br>
2024-10-20 19:26:24 -0300

    feat: ensure update planet throws ConflictError if planet is already registered

--- src/v1/service/impl/planet.service.impl.ts
+++ src/v1/service/impl/planet.service.impl.ts
@@ -33,10 +33,15 @@ export class  PlanetServiceImpl implements PlanetService{
 
     async update(planet: Planet): Promise<void> {
         const savedPlanet = await this.readById(planet.id)
+        const isValidName =  await this.planetRepository.findByName(savedPlanet.name)
+
+        if(isValidName != null) throw new ConflictError('Planet is already registered!')
+
         savedPlanet.name =  planet.name !== savedPlanet.name && planet.name !== "" ? planet.name : savedPlanet.name
         savedPlanet.climate = planet.climate !== savedPlanet.climate && planet.climate !== "" ? planet.climate : savedPlanet.climate
         savedPlanet.terrain =  planet.terrain !== savedPlanet.terrain && planet.terrain !== "" ? planet.terrain : savedPlanet.terrain
         savedPlanet.population = planet.population !== savedPlanet.population ? planet.population : savedPlanet.population
+
         await this.planetRepository.updatePlanet(savedPlanet)
         await redisClient.del('planets');
     }
