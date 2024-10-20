280481f3e323a6c206a4f46879d8496412808741
gervasioartur <gervasioartur@outlook.com.br>
2024-10-20 10:23:01 -0300

    test: read all planets returns 200 and list of planets on success

--- tests/v1/mocks/routes-mock.ts
+++ tests/v1/mocks/routes-mock.ts
@@ -3,12 +3,16 @@ import { PlanetController } from '@/v1/api/controllers/planet.controller';
 import { PlanetService } from '@/v1/service/contract/planet.service';
 import { Router } from 'express';
 
-export const mockPlanetService = { create: jest.fn() } as unknown as PlanetService;
+export const mockPlanetService =
+  { create: jest.fn(),
+    readAll: jest.fn()
+  } as unknown as jest.Mocked<PlanetService>;
 
 const routerMock = Router()
 const planetController = new PlanetController(mockPlanetService);
 
 // Planet Routes
 routerMock.post('/planets',createPlanetValidator, planetController.create)
+routerMock.get('/planets', planetController.readAll)
 
 export  default  routerMock
