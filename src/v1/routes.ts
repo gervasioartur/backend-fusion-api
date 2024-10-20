570d69671bf28af1cb620ad5fad0af51874960a6
gervasioartur <gervasioartur@outlook.com.br>
2024-10-20 07:29:23 -0300

    chore: configuring redis cache

--- src/v1/routes.ts
+++ src/v1/routes.ts
@@ -5,12 +5,6 @@ import { createPlanetValidator } from '@/v1/api/validators';
 const router = Router()
 const planetController = makePlanetController()
 
-
-// Home ROUTE
-router.get('/', (req: Request, res: Response, next: NextFunction) => {
-    res.send('Hello world version 1')
-});
-
 // Planet Routes
 router.post('/planets',createPlanetValidator, planetController.create)
 
