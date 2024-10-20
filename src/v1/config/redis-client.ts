976daa6b24742eaad46d67d3ada004d2481b1b09
gervasioartur <gervasioartur@outlook.com.br>
2024-10-20 15:39:23 -0300

    chore: changing redis configuration

--- src/v1/config/redis-client.ts
+++ src/v1/config/redis-client.ts
@@ -4,6 +4,4 @@ import * as dotenv from "dotenv";
 dotenv.config();
 const url = `redis://${process.env.REDIS_HOST}:${process.env.REDIS_PORT}`
 const redisClient = createClient({url})
-redisClient.on('error', (error) => console.log('Redis client Error: ', error))
-redisClient.connect();
 export  default  redisClient
