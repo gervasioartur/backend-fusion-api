82ae30045a00d7ec5fb16fd3629e885defb423e7
gervasioartur <gervasioartur@outlook.com.br>
2024-10-20 09:44:18 -0300

    feat: ensure find all returns planets

--- src/v1/persistence/repository/impl/planet.repository.impl.ts
+++ src/v1/persistence/repository/impl/planet.repository.impl.ts
@@ -4,10 +4,10 @@ import {Repository} from "typeorm";
 
 export  class PlanetRepositoryImpl extends Repository<Planet> implements PlanetRepository {
     async findByName(name: string): Promise<Planet | null> {
-        return this.findOne({ where: {  name, active: true } });
+        return await this.findOne({ where: {  name, active: true } });
     }
 
-    findAll(): Promise<Planet[]> {
-        return this.find({where: { active: true }});
+    async findAll(): Promise<Planet[]> {
+        return await this.find({where: { active: true }});
     }
 }
