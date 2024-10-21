87bf31691c18d0fdf2e0a9481e574da912715037
gervasioartur <gervasioartur@outlook.com.br>
2024-10-20 21:58:59 -0300

    chore: configuring infra and app deploy

---
+++ infra/varialbles.tf
@@ -0,0 +1,62 @@
+variable "environment" {
+   type = string
+}
+
+# Bucket variables
+variable "bucket_name" {
+   type = string
+}
+
+# Database instance variables
+variable "database_allocated_storage" {
+   type = number
+}
+
+variable "database_max_allocated_storage" {
+  type = number
+}
+
+variable "database_engine_version" {
+  type = string
+}
+
+variable "database_instance_class" {
+  type = string
+}
+
+variable "database_name" {
+  type = string
+}
+
+variable "database_password" {
+  type = string
+}
+
+variable "database_username" {
+  type = string
+}
+
+variable "database_parameter_group_name" {
+  type = string
+}
+
+variable "database_instance_name" {
+  type = string
+}
+
+# EC2 variables
+variable "deployer_key_name" {
+  type = string
+}
+
+variable "ami_instance" {
+  type = string
+}
+
+variable "ami_instance_type" {
+  type = string
+}
+
+variable "server_instance_name" {
+  type = string
+}
